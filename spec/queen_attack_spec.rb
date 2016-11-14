13c2784bdf24dca9b93826833f5b0723efc1f8d9
Cody Brubaker and Jim Padilla <cody@>
2016-11-14 14:09:11 -0800

    add final spec

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -3,9 +3,9 @@ require('queen_attack')
 
 describe('Array#queen_attack?') do
 
-  # it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
-  #   expect([1,1].queen_attack?([2,3])).to(eq(false))
-  # end
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([1,1].queen_attack?([2,3])).to(eq(false))
+  end
 
   it('will return true when queen is horizontal to enemy piece') do
     expect([3,0].queen_attack?([2, 0])).to(eq(true))
